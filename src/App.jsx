9af786f13cfe83283ae11c1a81b72011640a40c1
lavanyakamboj <kambojlavanya076@gmail.com>
2025-08-09 22:07:36 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
