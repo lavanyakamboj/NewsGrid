1dac0485c3b5064c368e4e16992ebf874ce96946
lavanyakamboj <kambojlavanya076@gmail.com>
2025-08-31 20:15:15 +0530

    modifications

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 // import url('https://fonts.googleapis.com/css2?family=Manufacturing+Consent&display=swap');
 
-export const API_KEY = "pub_1ada8fd428c34acea4a03a8d96d80ef5";
-export const API_KEY_One = "pub_70dbb75bd9674f84bc8fded9ec394498";    
-export const API_KEY_Two = "pub_d19d5cfd362d489a84b39e14c8f48765";        
+export const API_KEY = "pub_1ada8fd428c34acea4a03a8d96d80ef5";        // used for else part
+export const API_KEY_One = "pub_70dbb75bd9674f84bc8fded9ec394498";    // used for the search
+export const API_KEY_Two = "pub_d19d5cfd362d489a84b39e14c8f48765";    // extra
 
-export const Weather_API_KEY ="841de802c646478857ae6b8a783e0fd0";
+export const Weather_API_KEY ="841de802c646478857ae6b8a783e0fd0";     // weather api key
